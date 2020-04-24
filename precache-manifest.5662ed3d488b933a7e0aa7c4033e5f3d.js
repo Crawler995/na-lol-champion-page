dd5eccf74ad4aa55cc29a3a126054f52854bd1e7
crawler995 <zhang_995@foxmail.com>
2020-04-24 19:23:26 +0800

    Updates

---
+++ precache-manifest.5662ed3d488b933a7e0aa7c4033e5f3d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13b38157e721c7b35a631ef2d7feb47a",
+    "url": "/Crawler995/na-lol-champion-page/index.html"
+  },
+  {
+    "revision": "929e3872afde9fdb8a48",
+    "url": "/Crawler995/na-lol-champion-page/static/css/main.59c00c19.chunk.css"
+  },
+  {
+    "revision": "24313ec3bc9abb986421",
+    "url": "/Crawler995/na-lol-champion-page/static/js/2.5e1eb9f9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Crawler995/na-lol-champion-page/static/js/2.5e1eb9f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "929e3872afde9fdb8a48",
+    "url": "/Crawler995/na-lol-champion-page/static/js/main.68eda311.chunk.js"
+  },
+  {
+    "revision": "57fe6eff8e5f3e86254d",
+    "url": "/Crawler995/na-lol-champion-page/static/js/runtime-main.644201fd.js"
+  },
+  {
+    "revision": "4b2bd1003d4d48eaaad1c60d7a317693",
+    "url": "/Crawler995/na-lol-champion-page/static/media/BeaufortforLOL-Bold.4b2bd100.woff"
+  },
+  {
+    "revision": "1896a784b0bd4c3d5ddb20a9c9a1e935",
+    "url": "/Crawler995/na-lol-champion-page/static/media/BeaufortforLOL-HeavyItalic.1896a784.woff"
+  },
+  {
+    "revision": "6ba190062a273513e54d686f22bea68a",
+    "url": "/Crawler995/na-lol-champion-page/static/media/BeaufortforLOL-Regular.6ba19006.woff"
+  },
+  {
+    "revision": "f51629f2c912c71015e50134724acddb",
+    "url": "/Crawler995/na-lol-champion-page/static/media/Spiegel-Bold.f51629f2.woff"
+  },
+  {
+    "revision": "dd7486c605add1a5ce2c8b4fd3ada1dd",
+    "url": "/Crawler995/na-lol-champion-page/static/media/Spiegel-Regular.dd7486c6.woff"
+  }
+]);
